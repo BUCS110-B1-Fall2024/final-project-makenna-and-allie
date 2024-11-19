392ba62043bf88bef7817a71a8db2a1e559a930b
Allison Connolly <allieconnolly@Allisons-MacBook-Air.local>
2024-11-19 17:40:23 -0500

    class1

---
+++ template_final_project-master/src/character.py
@@ -0,0 +1,35 @@
+class Character:
+    import turtle
+    
+    def __init__ (self, x, y, img_file):
+        """ Initializes character (turtle) that will play game
+        args: 
+        - x : int - starting x coordinate
+        - y : int - starting y coordinate
+        - img_file : str - maze file
+        """
+        
+    def move_left(self):
+         """ Moves character left 1
+        args: None
+        return: None
+        """
+    
+    def move_right(self):
+        """ Moves character right 1
+        args: None
+        return: None
+        """
+        
+    def move_forward(self):
+        """ Moves character forward 1
+        args: None
+        return: None
+        """
+        
+    def move_backward(self):
+        """ Moves character back 1
+        args: None
+        return: None
+        """
+        
