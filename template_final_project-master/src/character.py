4986bf5b5a268331f3c08e7a521bd15cda62e947
Allison Connolly <allieconnolly@allisons-air.myfiosgateway.com>
2024-11-30 21:50:29 -0500

    progress

--- template_final_project-master/src/character.py
+++ template_final_project-master/src/character.py
@@ -8,28 +8,43 @@ class Character:
         - y : int - starting y coordinate
         - img_file : str - maze file
         """
+        self.turtle = turtle.Turtle()
+        self.turtle.goto(self.x, self.y)
+        self.file = img_file
         
     def move_left(self):
          """ Moves character left 1
         args: None
         return: None
         """
+        x_coordinate = self.turtle.xcor()
+        y_coordinate = self.turtle.ycor()
+        self.turtle.goto(x_coordinate-1, y_coordinate)
     
     def move_right(self):
         """ Moves character right 1
         args: None
         return: None
         """
+        x_coordinate = self.turtle.xcor()
+        y_coordinate = self.turtle.ycor()
+        self.turtle.goto(x_coordinate+1, y_coordinate)
         
     def move_forward(self):
         """ Moves character forward 1
         args: None
         return: None
         """
+        x_coordinate = self.turtle.xcor()
+        y_coordinate = self.turtle.ycor()
+        self.turtle.goto(x_coordinate, y_coordinate+1)
         
     def move_backward(self):
         """ Moves character back 1
         args: None
         return: None
         """
+        x_coordinate = self.turtle.xcor()
+        y_coordinate = self.turtle.ycor()
+        self.turtle.goto(x_coordinate, y_coordinate-1)
         
