9205f3d6bf0e135f2f9c3a286c3ae745fb30826c
Makenna Baier <makennabaier@Owners-MacBook-Pro.local>
2024-12-04 17:02:35 -0500

    updated

--- template_final_project-master/src/controller.py
+++ template_final_project-master/src/controller.py
@@ -1,7 +1,9 @@
-#import character 
-#import trivia 
 import pygame
-import setup
+from src import character 
+from src import trivia 
+from src import setup
+from src import maze
+
 
 class Controller:
     
@@ -18,15 +20,30 @@ class Controller:
         returns: string - if you won the game
         """
         #1- Handle events
-        
-        begin = setup.Setup("black")
+        screen = pygame.display.set_mode((1200,1200))
+        pygame.display.set_caption('Maze')
+        begin = setup.Setup("black", screen)
         running = True
+        
         begin.create()
+        pygame.display.flip()
         
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    screen = pygame.display.set_mode((1200,1200))
+                    screen.fill('black')
+                    grid = maze.Maze(5,5, screen, 50)
+                    grid.drawGrid()
+                    pygame.display.flip()
+                    
+                    
+                    
+                    
+            
+            
         #2. detect collisions and update models, ask character where
         # to move, etc.
         #3. Redraw next frame
