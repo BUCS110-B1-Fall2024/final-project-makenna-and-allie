09a86c954f2d91c7db7ab70e2d75f891486c759b
Allison Connolly <allieconnolly@Allisons-MacBook-Air.local>
2024-12-04 17:45:32 -0500

    update

--- template_final_project-master/src/character.py
+++ template_final_project-master/src/character.py
@@ -1,49 +1,53 @@
 class Character:
     import turtle
+    import pygame
     
-    def __init__ (self, x, y):
+    def __init__ (self, screen):
         """ Initializes character (turtle) that will play game
         args: 
-        - x : int - starting x coordinate
-        - y : int - starting y coordinate
+        screen (display)- screen of the maze
         """
-        self.turtle = turtle.Turtle()
-        self.turtle.goto(x, y)
-        self.turtle.x = x
-        self.turtle.y = y
-        
-    def move_left(self):
-        """ Moves character left 1
-        args: None
-        return: None
-        """
-        x, y = self.pos()
-        self.turtle.goto(x-1, y)
-    
-    def move_right(self):
-        """ Moves character right 1
-        args: None
-        return: None
-        """
-        x_coordinate = self.turtle.xcor()
-        y_coordinate = self.turtle.ycor()
-        self.turtle.goto(x_coordinate+1, y_coordinate)
+        self.turt = turtle.Turtle()
+        self.turtle.goto(0, 0)
+        self.screen = screen
         
     def move_forward(self):
         """ Moves character forward 1
         args: None
         return: None
         """
-        x_coordinate = self.turtle.xcor()
-        y_coordinate = self.turtle.ycor()
-        self.turtle.goto(x_coordinate, y_coordinate+1)
-        
-    def move_backward(self):
+        self.turtle.setheading(90)
+        self.turtle.forward(1)
+
+    def move_backwards(self):
         """ Moves character back 1
+            args: None
+            return: None
+            """
+        self.turtle.setheading(270)
+        self.turtle.forward(1)
+
+    def move_left(self):
+        """ Moves character left 1
         args: None
         return: None
         """
-        x_coordinate = self.turtle.xcor()
-        y_coordinate = self.turtle.ycor()
-        self.turtle.goto(x_coordinate, y_coordinate-1)
-        
+        self.turtle.setheading(180)
+        self.turtle.forward(1)
+
+    def move_right(self):
+        """ Moves character right 1
+        args: None
+        return: None
+        """
+        self.turtle.setheading(0)
+        self.turtle.forward(1)
+
+    self.screen.listen()
+    self.screen.onkey(move_forward, "Up")
+    self.screen.onkey(move_backwards, "Down")
+    self.screen.onkey(move_left, "Left")
+    self.screen.onkey(move_right, "Right")
+
+    self.screen.mainloop()
+            
