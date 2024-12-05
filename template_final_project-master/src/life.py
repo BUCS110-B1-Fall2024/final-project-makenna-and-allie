f3895f4ad40f9ae55dc47f7a7362386d27b853a5
Allison Connolly <allieconnolly@Allisons-MacBook-Air.local>
2024-12-05 14:53:55 -0500

    m

--- template_final_project-master/src/life.py
+++ template_final_project-master/src/life.py
@@ -1,9 +1,18 @@
+import pygame
+
 class Life():
-    def __init__(self):
+    def __init__(self, screen, max_lives=2):
+        """ Initializes life system for the character
+        args: screen (display) - creates the screen 
+        max_lives (int) - initializes the number of 
+        lives the character starts with
+        """
+        self.screen = screen
+        self.max_lives = max_lives
+        self.current_lives = max_lives
+        
+    def lose_life(self):
         self.x
         
-    def check4rectangles(self):
-         self.x
+    def lose_game(self):
         
-    def update_lives(self):
-        self.x
