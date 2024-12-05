1b1b712bdfe58b657143620c9252fd2b28945858
Makenna Baier <makennabaier@Owners-MacBook-Pro.local>
2024-12-04 22:04:36 -0500

    updated

--- template_final_project-master/src/maze.py
+++ template_final_project-master/src/maze.py
@@ -1,20 +1,39 @@
 import pygame
-from src import button
+from src import cell
 
 class Maze:
     
-    def __init__(self, rows, cols, screen, size):
+    def __init__(self, rows, cols, screen, size, border):
         self.row = rows
         self.col = cols
         self.screen = screen
-        self.color = 'white'
+        self.color = 'blue'
+        self.bord_col = border
         self.size = size
+        self.dir = 90
+        self.list = []
         
-    def drawGrid(self):
-        for x in range(self.size, self.screen.get_width(), self.size):
-            pygame.draw.line(self.screen, self.color, (x, 0), (x, self.screen.get_height()),2)
-        for y in range(self.size, self.screen.get_height(), self.size):
-            pygame.draw.line(self.screen, self.color, (0, y), (self.screen.get_width(), y),2)
+    def drawGrid(self):   
+        
+        num = 1
+        grid_size = int((self.screen.get_height() - (2 * self.size)))
+        for x in range(0, self.screen.get_width(), self.size):
+            for y in range(0, grid_size, self.size):
+                box = cell.Cell(x, y, self.screen, False, num, self.size)
+                num += 1
+                self.list.append(box)
+                box.draw()
+                box.get_num()
+                
+                
+                
+                
+                
+                
+       # current_x = 0
+        #current_y = 0
+        #box = cell.Cell(current_x, current_y, self.screen, True)
+        #box.draw(self.size)
     
             
             
