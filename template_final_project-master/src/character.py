bcdfc6592ddedf6ec99abf0efa8ceacd9e7005df
Makenna Baier <makennabaier@Owners-MacBook-Pro.local>
2024-12-05 23:29:43 -0500

    done

--- template_final_project-master/src/character.py
+++ template_final_project-master/src/character.py
@@ -2,7 +2,7 @@ import turtle
 import pygame
 
 class Character(pygame.sprite.Sprite):
-    def __init__ (self, screen, color, w, h):
+    def __init__ (self, screen, color, x, y):
         """ Initializes character (turtle) that will play game
         args: 
         screen (display) - screen of the maze
@@ -10,58 +10,37 @@ class Character(pygame.sprite.Sprite):
         """
         super().__init__()
         self.screen = screen
-        self.image = pygame.Surface([w,h])
+        self.size = 20
+        self.image = pygame.Surface([self.size, self.size])
         self.image.fill(color)
 
         self.color = color
-        self.x = 10
-        self.y = 500
-        pygame.draw.rect(self.image, color, pygame.Rect(self.x, self.y, w, h))
+       
+        pygame.draw.rect(self.image, color, pygame.Rect(x, y, x, y))
         self.rect = self.image.get_rect()
+        #self.rect.x = x
+        #self.rect.y = y
         
-<<<<<<< HEAD
-       # self.screen.listen()
-        #self.screen.onkey(self.move_forward, "Up")
-        #self.screen.onkey(self.move_backwards, "Down")
-        #self.screen.onkey(self.move_left, "Left")
-        #self.screen.onkey(self.move_right, "Right")
+    #def update(self):
+        #self.velocity += self.gravity
+        #self.rect.y += self.velocity
         
-        #self.move_forward()
+        
     
-=======
-        self.gravity = 1
-        self.velocity = 0
-        self.jump_strength = -15
-        self.is_jumping = False
         
     def create_character(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
         
->>>>>>> 4acec5b9be21dddafddba2f8b284b38454281166
-    def move_forward(self):
+    def move(self,key):
         """ Moves character forward repeatedly
         args: None
         return: None
         """
-        self.velocity += self.gravity
-        self.rect.y += self.velocity
-
-        if self.rect.bottom > self.screen.get_height():
-            self.rect.bottom = self.screen.get_height()
-
-        if self.rect.top < 0:
-            self.rect.top = 0
-            
-    def move_up(self):
-        """ Moves character up 1
-        args: None
-        return: None
-        """
-        if not self.is_jumping: 
-            self.velocity = self.jump_strength
-            self.is_jumping = True
-
-
-    #def redraw(self):
+        if key[pygame.K_RIGHT]:
+            self.rect.x += 15   # Move right
+        if key[pygame.K_UP]:
+            self.rect.y -= 15  # Move up
+        if key[pygame.K_DOWN]:
+            self.rect.y += 15   # Move down
         
 
